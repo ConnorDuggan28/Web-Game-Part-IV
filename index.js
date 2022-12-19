34afc41b96b937bf801526371d515013e563d8a0
Connor Duggan <connor.duggan28@yahoo.com>
2022-12-19 16:41:59 -0500

    changed function

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ const character = newImage('assets/green-character/static.gif')
 let direction = null;
 let x = 100;
 let y = 250;
+move(character).to(100, 250)
 
-function moveCharacter(){
-if(direction === 'west') {
-    x = x - 1
-}
-if(direction === 'north'){
-    y = y + 1
-}
-if(direction === 'east'){
-    x = x + 1
-}
-if(direction === 'south'){
-    y = y - 1
-}
-character.style.left = x + 'px'
-character.style.bottom = y + 'px'
-
-}
-setInterval(moveCharacter, 1)
+setInterval(function(){ 
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}, 1)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
